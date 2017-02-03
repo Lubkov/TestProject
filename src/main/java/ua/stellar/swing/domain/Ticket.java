b2e939d80619449e7cbf8d8409404db514ff6656
Lubkov <ser.lubkov@gmail.com>
2017-02-03 22:29:01 +0200

    3 commit

--- src/main/java/ua/stellar/swing/domain/Ticket.java
+++ src/main/java/ua/stellar/swing/domain/Ticket.java
@@ -1,5 +1,37 @@
 package ua.stellar.swing.domain;
 
 public class Ticket {
+	
+	private int id;
+	private String name;
+	private float price;
+	
+	public Ticket() {
+		
+	}
 
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public float getPrice() {
+		return price;
+	}
+
+	public void setPrice(float price) {
+		this.price = price;
+	}
+	
 }
